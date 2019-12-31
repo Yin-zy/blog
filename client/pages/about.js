4637cb7c3071e30615fb9fd54949f43a0ae40683
Yin-zy <909472777@qq.com>
2019-12-31 18:09:19 +0800

    c

--- client/pages/about.js
+++
@@ -1,8 +0,0 @@
-import Header from "../components/header";
-const about = () => {
-  <>
-    <Header />
-    <h1>Hello World!</h1>
-  </>;
-};
-export default about;
--- client/pages/index.js
+++ client/pages/index.js
@@ -1,8 +1,4 @@
-import Header from "../components/header";
 const home = () => {
-  <>
-    <Header />
-    <h1>Hello World!</h1>
-  </>;
+  <h1>Hello World!</h1>;
 };
 export default home;
