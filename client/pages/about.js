b306ef07772a5c6cc35a0ceba8939b0d990b0000
Yin-zy <909472777@qq.com>
2019-12-31 14:20:13 +0800

    the first try of next js

---
+++ client/pages/about.js
@@ -0,0 +1,8 @@
+import Header from "../components/header";
+const about = () => {
+  <>
+    <Header />
+    <h1>Hello World!</h1>
+  </>;
+};
+export default about;
---
+++ client/pages/index.js
@@ -0,0 +1,8 @@
+import Header from "../components/header";
+const home = () => {
+  <>
+    <Header />
+    <h1>Hello World!</h1>
+  </>;
+};
+export default home;
